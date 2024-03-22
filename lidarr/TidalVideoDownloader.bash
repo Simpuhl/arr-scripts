ba948c458db563a6edad1cd068656fac6a420465
Kevin Still <simpuhl@hotmail.com>
2024-03-22 13:28:24 -0700

    Update TidalVideoDownloader.bash

--- lidarr/TidalVideoDownloader.bash
+++ lidarr/TidalVideoDownloader.bash
@@ -3,9 +3,9 @@ scriptVersion="1.9"
 scriptName="TidalVideoDownloader"
 
 #### Import Settings
-source /config/extended.conf
+source /var/lib/lidarr/config/extended.conf
 #### Import Functions
-source /config/extended/functions
+source /var/lib/lidarr/config/extended/functions
 
 
 verifyConfig () {
