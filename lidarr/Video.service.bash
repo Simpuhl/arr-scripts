0396e822ecb824acb47f8a41e5ecb033729c3cce
Kevin Still <simpuhl@hotmail.com>
2024-03-22 13:28:51 -0700

    Update Video.service.bash

--- lidarr/Video.service.bash
+++ lidarr/Video.service.bash
@@ -3,9 +3,9 @@ scriptVersion="3.7"
 scriptName="Video"
 
 ### Import Settings
-source /config/extended.conf
+source /var/lib/lidarr/config/extended.conf
 #### Import Functions
-source /config/extended/functions
+source /var/lib/lidarr/config/extended/functions
 
 verifyConfig () {
 	if [ -z "$videoContainer" ]; then
