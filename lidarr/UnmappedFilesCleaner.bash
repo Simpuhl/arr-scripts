cea7bb7360ba38d320d419c35ff9cb9e7e32acea
Kevin Still <simpuhl@hotmail.com>
2024-03-22 13:28:41 -0700

    Update UnmappedFilesCleaner.bash

--- lidarr/UnmappedFilesCleaner.bash
+++ lidarr/UnmappedFilesCleaner.bash
@@ -3,9 +3,9 @@ scriptVersion="1.3"
 scriptName="UnmappedFilesCleaner"
 
 #### Import Settings
-source /config/extended.conf
+source /var/lib/lidarr/config/extended.conf
 #### Import Functions
-source /config/extended/functions
+source /var/lib/lidarr/config/extended/functions
 
 verifyConfig () {
   if [ "$enableUnmappedFilesCleaner" != "true" ]; then
