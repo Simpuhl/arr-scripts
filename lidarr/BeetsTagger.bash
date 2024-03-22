da13d37b533e8412708c97f04ca55df1de74fb2f
Kevin Still <simpuhl@hotmail.com>
2024-03-22 13:27:41 -0700

    Update BeetsTagger.bash

--- lidarr/BeetsTagger.bash
+++ lidarr/BeetsTagger.bash
@@ -3,9 +3,9 @@ scriptVersion="1.8"
 scriptName="BeetsTagger"
 
 #### Import Settings
-source /config/extended.conf
+source /var/lib/lidarr/config/extended.conf
 #### Import Functions
-source /config/extended/functions
+source /var/lib/lidarr/config/extended/functions
 #### Create Log File
 logfileSetup
 
