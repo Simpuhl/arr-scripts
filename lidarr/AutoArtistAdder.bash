b988afa24a87440029d825757fb503dae8046f87
Kevin Still <simpuhl@hotmail.com>
2024-03-22 13:27:15 -0700

    Update AutoArtistAdder.bash

--- lidarr/AutoArtistAdder.bash
+++ lidarr/AutoArtistAdder.bash
@@ -3,9 +3,9 @@ scriptVersion="2.1"
 scriptName="AutoArtistAdder"
 
 ### Import Settings
-source /config/extended.conf
+source /var/lib/lidarr/config/extended.conf
 #### Import Functions
-source /config/extended/functions
+source /var/lib/lidarr/config/extended/functions
 
 verifyConfig () {
  
